7fa6a8db72263c40c81aa35c9b9f966231e9911c
KotikGamerz <162711066+KotikGamerz@users.noreply.github.com>
2025-11-28 21:23:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Grow A Garden API is running!"
+
 @app.route("/stock")
 def stock():
     return jsonify({
         "seeds": [
             "Watermelon x5",
             "Tomato x1",
-            "Blueberry x2"
+            "Blueberry x2",
+            "Strawberry x3"
         ],
         "gear": [
             "Basic Sprinkler x3",
@@ -16,9 +21,11 @@ def stock():
         ],
         "eggs": [
             "Uncommon Egg x1",
-            "Rare Egg x1"
+            "Rare Egg x1",
+            "Epic Egg x1"
         ]
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    # Very important for Render
+    app.run(host="0.0.0.0", port=10000)
